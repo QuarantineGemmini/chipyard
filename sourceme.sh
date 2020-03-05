10958424bfed13f136274f5a324b08b67522f0f5
Sam Steffl <sam@ssteffl.com>
2020-03-05 07:58:40 -0800

    local spike gemmini sims work

---
+++ sourceme.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# TODO:!!!!! don't let the buildsim_gemmini* scripts source this sourceme...
+
+DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+CURHOST="$(hostname -f | awk '{print tolower($0)}')"
+
+if [[ "$CURHOST" =~ ^eda-.*.eecs.berkeley.edu$ ]] ; then
+  #---------------------------------------------------------------------------
+  # cory eda-* machines
+  #---------------------------------------------------------------------------
+  source /home/ff/ee290-2/chipyard-env.sh
+
+  # for viewing rtl schematic in dc_shell
+  export PATH="$PATH:/share/instsww/synopsys/J-2014.09-SP3/bin"
+
+  # use for debug-spike builds
+  #source /home/ff/ee290-2/chipyard-debug-env.sh
+else
+  #---------------------------------------------------------------------------
+  # local-machine builds
+  #---------------------------------------------------------------------------
+  source $DIR/env.sh
+fi
+
+# add convenience gemmini scripts
+export PATH="$PATH:$DIR/generators/gemmini/software/sim_scripts"
+
